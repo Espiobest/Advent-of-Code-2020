dfc873bd43510e67a0a9e9f3f7fc7a00c84cdd92
Espiobest <59823894+Espiobest@users.noreply.github.com>
2020-12-02 02:22:30 +0530

    Update Day1.py

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -6,9 +6,9 @@ from itertools import permutations
 numbers = [*map(int,c)]
 target_number = 2020
 
-nums = (list(permutations(numbers, 3)))
+nums = (list(permutations(numbers, 2)))
 
-result1 = [i for i in nums if i[0]+i[1]+i[2] == target_number] #part 1
+result1 = [i for i in nums if i[0]+i[1] == target_number] #part 1
 print(result1)
 
 nums = (list(permutations(numbers, 3)))
