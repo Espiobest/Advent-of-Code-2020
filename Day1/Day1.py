70946bc5c81748dd44673af36b0d9806af710d3a
Ayush Ravichandran <ayushravichandran@gmail.com>
2020-12-02 02:06:11 +0530

    Day 1 solution

---
+++ Day1/Day1.py
@@ -0,0 +1,17 @@
+r = open('data.txt')
+c = r.read().strip().split()
+numbers = [*map(int,c)]
+from itertools import permutations
+
+numbers = [*map(int,c)]
+target_number = 2020
+
+nums = (list(permutations(numbers, 3)))
+
+result1 = [i for i in nums if i[0]+i[1]+i[2] == target_number] #part 1
+print(result1)
+
+nums = (list(permutations(numbers, 3)))
+
+result2 = [i for i in nums if i[0]+i[1]+i[2] == target_number] #part 2
+print(result2)
