d21bf8fa3001073f6ac9a8238cbef9e465e8ae5a
Espiobest <59823894+Espiobest@users.noreply.github.com>
2020-12-02 02:26:05 +0530

    Update Day1.py

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,17 +1,18 @@
+from itertools import permutations
+
+
 r = open('data.txt')
 c = r.read().strip().split()
-numbers = [*map(int,c)]
-from itertools import permutations
 
 numbers = [*map(int,c)]
 target_number = 2020
 
-nums = (list(permutations(numbers, 2)))
+nums1 = (list(permutations(numbers, 2)))
 
 result1 = [i for i in nums if i[0]+i[1] == target_number] #part 1
 print(result1)
 
-nums = (list(permutations(numbers, 3)))
+nums2 = (list(permutations(numbers, 3)))
 
 result2 = [i for i in nums if i[0]+i[1]+i[2] == target_number] #part 2
 print(result2)
