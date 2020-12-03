b87acf88306e5feb6fb609069174ee44fa70e077
Ayush Ravichandran <ayushravichandran@gmail.com>
2020-12-03 10:57:24 +0530

    Day 3 solution

---
+++ Day3/Day3.py
@@ -0,0 +1,16 @@
+z = open('data.txt')
+F = z.read().strip().split()
+lst = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+fg = []
+k = 1
+for a,b in lst:
+    z = c = s = 0
+    while z < len(F):
+        c += a
+        z += b
+        if F[z][c%len(F[z])]== '#' and z<len(F):
+            s+=1
+    k *= s
+    if a == 3 and b == 1:
+        print(s)
+print(k)
