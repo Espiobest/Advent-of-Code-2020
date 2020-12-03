5e4158b5a6bee11a40386acd4868b5bc5a578a8e
Espiobest <59823894+Espiobest@users.noreply.github.com>
2020-12-03 14:54:17 +0530

    Update Day1.py

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -9,10 +9,10 @@ target_number = 2020
 
 nums1 = (list(permutations(numbers, 2)))
 
-result1 = [i for i in nums if i[0]+i[1] == target_number] #part 1
+result1 = [i for i in nums1 if i[0]+i[1] == target_number] #part 1
 print(result1)
 
 nums2 = (list(permutations(numbers, 3)))
 
-result2 = [i for i in nums if i[0]+i[1]+i[2] == target_number] #part 2
+result2 = [i for i in nums2 if i[0]+i[1]+i[2] == target_number] #part 2
 print(result2)
